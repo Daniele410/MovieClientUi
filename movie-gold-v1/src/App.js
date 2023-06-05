afd38fc370e1b8581eb234a717b09c9f6435a762
Daniele410 <miraglia.daniele410@gmail.com>
2023-06-05 17:32:26 +0200

    add Hero component

--- movie-gold-v1/src/App.js
+++ movie-gold-v1/src/App.js
@@ -1,59 +1,66 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
   const [movies, setMovies] = useState();
-  const [movie, setMovie] = useState();
-  const [reviews, setReviews] = useState([]);
+  // const [movie, setMovie] = useState();
+  // const [reviews, setReviews] = useState([]);
 
-  const getMovies = async () =>{
-    
-    try
-    {
+  const getMovies = async () => {
+
+    try {
 
       const response = await api.get("/api/v1/movies");
 
-      console.log(response.data);
+      // console.log(response.data);
 
       setMovies(response.data);
 
-    } 
-    catch(err)
-    {
+    }
+    catch (err) {
       console.log(err);
     }
   }
 
-  const getMovieData = async (movieId) => {
-     
-    try 
-    {
-        const response = await api.get(`/api/v1/movies/${movieId}`);
+  // const getMovieData = async (movieId) => {
 
-        const singleMovie = response.data;
+  //   try {
+  //     const response = await api.get(`/api/v1/movies/${movieId}`);
 
-        setMovie(singleMovie);
+  //     const singleMovie = response.data;
 
-        setReviews(singleMovie.reviews);
-        
+  //     setMovie(singleMovie);
 
-    } 
-    catch (error) 
-    {
-      console.error(error);
-    }
+  //     setReviews(singleMovie.reviews);
 
-  }
+
+  //   }
+  //   catch (error) {
+  //     console.error(error);
+  //   }
+
+  // }
 
   useEffect(() => {
     getMovies();
-  },[])
+  }, [])
 
   return (
     <div className="App">
 
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />} ></Route>
+
+
+
+        </Route>
+      </Routes>
     </div>
   );
 }
